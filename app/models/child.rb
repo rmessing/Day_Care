6a066657653c2188fd78fed97e1503614435291c
Richard Messing <rsmessing@gmail.com>
2016-10-27 15:28:17 -0400

    Updated readme file

--- app/models/child.rb
+++ app/models/child.rb
@@ -3,7 +3,7 @@ class Child < ActiveRecord::Base
 	has_many :handoffs
 	belongs_to :group
 	has_many :families
-	has_many :parents, :through => :families, dependent: :destroy
+	has_many :parents, :through => :families
 	validates_presence_of :fname, :lname, on: :create, presence: true
 	
 end
